52f3811ed6426d7b857be68904b5a2a3747bc5ab
garwey@126.com <garwey@126.com>
2017-07-04 21:42:59 +0800

    first edition.

---
+++ src/CalculateTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+	Calculate calculation = new Calculate();
+	int sum = calculation.sum(2, 5);
+	int testSum = 7;
+
+	@Test
+	public void testSum() {
+		System.out.println("@Test sum(): " + sum + " = " + testSum);
+		assertEquals(sum, testSum);
+	}
+}
